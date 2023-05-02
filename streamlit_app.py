89f036d696e6838a19eee81cf8e6e6ce55f638ca
Noah Fuery <72906659+nfuery@users.noreply.github.com>
2023-05-02 16:06:01 -0700

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,8 @@
 
 import streamlit as st
 
-st.write("DB username:", st.secrets["db_username"])
-st.write("DB password:", st.secrets["db_password"])
-st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
+st.write("DB username:", st.secrets["username"])
+st.write("DB password:", st.secrets["password"])
 
 # Initialize connection.
 conn = st.experimental_connection('mysql', type='sql')
