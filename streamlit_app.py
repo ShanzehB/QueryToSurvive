8b3f647f920717ffefec8276536b632bfeac0c16
Noah Fuery <72906659+nfuery@users.noreply.github.com>
2023-05-02 16:07:13 -0700

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,15 +4,15 @@ import streamlit as st
 
 print(st.secrets)
 
-st.write("DB username:", st.secrets["username"])
-st.write("DB password:", st.secrets["password"])
+# st.write("DB username:", st.secrets["username"])
+# st.write("DB password:", st.secrets["password"])
 
-# Initialize connection.
-conn = st.experimental_connection('mysql', type='sql')
+# # Initialize connection.
+# conn = st.experimental_connection('mysql', type='sql')
 
-# Perform query.
-df = conn.query('SELECT * from mytable;', ttl=600)
+# # Perform query.
+# df = conn.query('SELECT * from mytable;', ttl=600)
 
-# Print results.
-for row in df.itertuples():
-    st.write(f"{row.name} has a :{row.pet}:")
+# # Print results.
+# for row in df.itertuples():
+#     st.write(f"{row.name} has a :{row.pet}:")
