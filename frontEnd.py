98667d89a3004bf1d2e432d76bd5b91a02cecd01
Noah Fuery <72906659+nfuery@users.noreply.github.com>
2023-05-02 18:02:46 -0700

    BigChange

--- frontEnd.py
+++ frontEnd.py
@@ -1,13 +1,4 @@
-# streamlit_app.py
-
 import streamlit as st
 
-# Initialize connection.
-conn = st.experimental_connection('mysql', type='sql')
+st.title('Query To Survive')
 
-# Perform query.
-df = conn.query('SELECT * from drivers;', ttl=600)
-
-# Print results.
-for row in df.itertuples():
-    st.write(f"{row.forename} {row.surname} is: {row.nationality}")
